a4fe775718ee9ef3fa69c1bc7112ea3689eaadf8
Danish Khan <danishkhan@zapbuild.com>
2020-08-18 13:04:23 +0530

    Initial Commit

---
+++ Firebase_Phone_Authentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase_Phone_Authentication
+//
+//  Created by Danish Khan on 18/08/20.
+//  Copyright © 2020 Danish Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
